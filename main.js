c3dd479ea9d6968dffef6923776539bba2657f1d
GabsNishikawa <144823271+GabsNishikawa@users.noreply.github.com>
2025-04-25 12:15:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-10-05T00:00:01");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:01");
+const tempoObjetivo3 = new Date("2025-12-30T00:00:01");
+const tempoObjetivo4 = new Date("2025-12-01T00:00:01");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
@@ -34,7 +34,8 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    if (tempoFinal > 0) {
+    if (tempoFinal > 0 )
+         {
         return [dias, horas, minutos, segundos];
     } else {
         return [0, 0, 0, 0];
